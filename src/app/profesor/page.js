989b8ce1a61756a3e1aebd12c91703e28dcaef4c
PSEC-jpg <ps2714926@gmail.com>
2024-11-15 17:37:21 -0400

    agregando rutas para el rpdofesor

---
+++ src/app/profesor/page.js
@@ -0,0 +1,8 @@
+export default function Profesor() {
+    return (
+      <div>
+        <h1>Página del Profesor</h1>
+        <p>Contenido para la página del profesor.</p>
+      </div>
+    );
+  }
